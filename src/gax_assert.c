96d9a4271c0a0ccba6a59da2ac57700aab6b3986
beanie_axolotl <tayloreton15@gmail.com>
2025-06-28 00:02:20 -0400

    update gax_assert functions
    > and document GAX_ASSERT some more

--- src/gax_assert.c
+++ src/gax_assert.c
@@ -1,13 +1,13 @@
 // void GAX_ASSERT_PRINT
 // https://decomp.me/scratch/nTYY5 - beanieaxolotl
-// accuracy -> 63.45%
+// accuracy -> 63.68%
 
 void GAX_ASSERT_PRINT(int x, int y, const char* string) {
 
-    u16 *vram_offset;
+    u16 *vram_offset = (u16*)VRAM;
     u8  letter;
-    u32 offset;
     u16 mapped_letter;
+    u32 offset;
     int i;
     
     vram_offset = (u16*)(VRAM + (x*2 + y*64));
@@ -22,9 +22,10 @@ void GAX_ASSERT_PRINT(int x, int y, const char* string) {
         }
         i = 0;
         offset        = ((*vram_offset & 0x3f)>>1);
-        mapped_letter = *string;
+        mapped_letter = string[i];
         
         if (offset < 32) {
+            
             while (TRUE) {
                 
                 if (letter == 0x00 || letter == ' ' || letter == '\n') {
@@ -56,8 +57,8 @@ void GAX_ASSERT_PRINT(int x, int y, const char* string) {
             }     
             if (letter == '\n') {
                 // new line
-                vram_offset = (u16*)((u32)offset & 0xffffffc0) + 0x3F;
-            }           
+                vram_offset = (u16*)((u32)offset & ~0x3F) + 0x3F;
+            }          
         
             if (letter == ' ') {
                 // space
@@ -66,10 +67,8 @@ void GAX_ASSERT_PRINT(int x, int y, const char* string) {
                 // lowercase to uppercase handler
                 if (letter < 'A') {
                     letter -= 0x2F;
+                    goto process_text;
                 } else if (letter < 'a') {
-                    if (letter < 'A') {
-                        goto process_text;
-                    }
                     letter -= 0x36;
                 } else {
                     letter -= 0x56;
@@ -95,9 +94,10 @@ void GAX_ASSERT(const char* fn, const char* msg) {
     
     // clear registers
     REG_IME       = 0;
-    REG_DMA1CNT_H = DMA_DEST_FIXED | DMA_32BIT | DMA_REPEAT;
-    REG_DMA2CNT_H = DMA_DEST_FIXED | DMA_32BIT | DMA_REPEAT;
-    REG_DMA3CNT_H = DMA_DEST_FIXED | DMA_32BIT | DMA_REPEAT;
+    REG_DMA1CNT_H = DMA_DEST_FIXED | DMA_32BIT | DMA_REPEAT; // initialize DMA 1
+    REG_DMA2CNT_H = DMA_DEST_FIXED | DMA_32BIT | DMA_REPEAT; // initialize DMA 2
+    REG_DMA3CNT_H = DMA_DEST_FIXED | DMA_32BIT | DMA_REPEAT; // initialize DMA 3
+    // ...and attempts to clear an undefined register for some reason
     (*(u16 *)0x40000EA) = DMA_DEST_FIXED | DMA_32BIT | DMA_REPEAT;
 
     CpuFastSet(0, (void*)VRAM, 0x1004000);
@@ -110,6 +110,14 @@ void GAX_ASSERT(const char* fn, const char* msg) {
     (*(u32*)(VRAM+0x44D4)) = 0x10000;    // colon (bottom dot)
     (*(u32*)(VRAM+0x44FC)) = 0x01111110; // underscore
 
+    // the (non-uppercase) date seems to be a preprocessor macro.
+    // the real string may be this:
+    /*
+        "GAX ENGINE V3.05A"__DATE__"\n\nEXCEPTION. PROGRAM HALT."
+    */
+    // but since this is currently matching the version from August of 2004
+    // this is not what we do
+    
     GAX_ASSERT_PRINT(0,0,"GAX ENGINE V3.05A Aug 16 2004\n\nEXCEPTION. PROGRAM HALT.");
     GAX_ASSERT_PRINT(0,5,"FUNCTION NAME:");
     GAX_ASSERT_PRINT(15,5,fn);
@@ -117,9 +125,10 @@ void GAX_ASSERT(const char* fn, const char* msg) {
 
     while (TRUE) {
         while(!(pressed_keys & START_BUTTON)) {
+            // what keys are currently being pressed?
             pressed_keys = REG_KEYINPUT ^ KEYS_MASK;           
         }
+        // soft reset the system if the start button is pressed
         SoftReset(0);
     }
-
 }
