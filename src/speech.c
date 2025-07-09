589803a74c5a90cd7fdb9802c70ff5fcfa83a0ee
beanie_axolotl <tayloreton15@gmail.com>
2025-07-09 13:35:05 -0400

    start work on speech.c
    > GAXSpeech_internal1 is just a cleaned-up Ghidra output for now

--- src/speech.c
+++ src/speech.c
@@ -5,8 +5,51 @@ void GAXSpeech_internal0() {
 
 }
 
-void GAXSpeech_internal1() {
-	
+// void GAXSpeech_internal1
+// https://decomp.me/scratch/ALcRA - beanieaxolotl
+// accuracy -> 84.15%
+
+void GAXSpeech_internal1(s16* param1) {
+    
+    int i;
+    s16 sVar1, iVar2;
+    
+    for (i = 7; i > -1; i--) {
+        
+        if (*param1 < 0) {
+            
+            if (*param1 == -32768) {
+                sVar1 = 32767;
+            } else {
+                sVar1 = -*param1;
+            }
+
+            if (sVar1 < 0x2B33) {
+                sVar1 *= -2;
+            } else if (sVar1 < 0x4E66) {
+                sVar1 = -(sVar1 + 0x2B33);
+            } else {
+                sVar1 = -((sVar1 >> 2) + 0x6600);
+            }
+            
+        } else {
+
+            sVar1 = *param1;
+            iVar2 = sVar1;
+            if (iVar2 < 0x2B33) {
+                sVar1 = iVar2<<1;
+            } else if (iVar2 < 0x4E66) {
+                sVar1 += 0x2B33;
+            } else {
+                sVar1 = (sVar1>>2)+0x6600;
+            }
+        }
+        
+        *param1 = sVar1;
+        *param1++;
+        
+    }
+    
 }
 
 void GAXSpeech_internal3() {
