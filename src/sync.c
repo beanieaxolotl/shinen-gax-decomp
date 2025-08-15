31907bd04ea7c980500510c63ea82f481cb27313
beanie_axolotl <tayloreton15@gmail.com>
2025-08-14 23:28:44 -0400

    update GAXSync_render

--- src/sync.c
+++ src/sync.c
@@ -32,65 +32,69 @@ void GAXSync_open(GAX_player* player) {
 
 // void GAXSync_render
 // https://decomp.me/scratch/QBX05 - beanieaxolotl
-// accuracy -> 90.66%
+// accuracy -> 94.12%
 
-void GAXSync_render(GAX_player* player, int unk) {
+u32 GAXSync_render(GAX_player* player, int unk) {
 
     GAXSongData* song;
     
-    u16 speed;
     int new_speed;
     u8  speed_timer;
     u16 speed_buffer;
     
     if (player->timer_backup != unk && player->is_playing) {
 
-        speed_buffer = player->speed_buf;
         speed_timer  = player->speed_timer;
-        
-        if (speed_buffer == 0 || (!player->songend, 
-            player->speed_timer != 0, player->speed_timer <= 0)) {     
+        speed_buffer = player->speed_buf;
+
+        // todo: if-statement is incorrect
+        if ((u8)player->speed_buf == 0 || (!player->songend, player->speed_timer <= 0)) {     
             
             // start of a step
             if (player->skip_pattern) {
                 // the song encounters a pattern break effect
                 player->skip_pattern = FALSE;
                 player->step = player->song->step_count; // load the last step + 1
+                song = player->song;                     // reload the song for the step data
             } else {
                 player->step++;      // load the next step
                 song = player->song; // reload the song for the step data
             }
 
-            speed = player->speed_buf >> 8;
-            if (speed != 0) {
-                player->speed_buf = speed | ((player->speed_buf & 0xFF) << 8);
+            if ((player->speed_buf >> 8) != 0) { 
+                // if the leftwards speed byte is not 0 (i.e end of a song),
+                // swap the speed buffer bytes to allow for speed modulation
+                player->speed_buf = speed_buffer | ((speed_buffer & 0xFF) << 8);
             }
-            
-            new_speed = GAX_ram->params->speed_adjust + ((u8)player->speed_buf-1);
-            if (new_speed < 0) {
-                new_speed = 0;
-            }
-            player->speed_timer = new_speed;
 
-            if ((s16)player->step >= song->step_count) {
-                
-                player->step             = 0;
-                player->new_step_idx     = 0;
-                player->pattern_finished = TRUE;
+            // adjust the speed via GAXParams->speed_adjust
+            new_speed = GAX_ram->params->speed_adjust + ((u8)player->speed_buf-1);
+            player->speed_timer = GAX_CLAMP(new_speed, 0, new_speed);
+            
+            if ((s16)player->step >= player->song->step_count) {
+                // if there are no more steps to process
+                player->step             = 0;    // restart from step #0
+                player->new_step_idx     = 0;    // same here
+                player->pattern_finished = TRUE; // pattern is now finished
                 player->pattern++;
                 
                 if (song->song_length >= player->pattern) {
+                    // if there are no more patterns to process
                     if (player->stop_on_songend) {
+                        // handler for the GAX_STOP_ON_SONGEND flag
                         player->is_playing = FALSE;
                         player->speed_buf  = 0;
                     }
                     player->songend = TRUE;
                     player->pattern = song->restart_position;
+                    
                 } else {
+                    // when a pattern simply finishes 
+                    // and there are more patterns after it
                     player->pattern_finished = TRUE;
                 }
             }
-            player->step_finished = TRUE;            
+            player->step_finished = TRUE;     
             
         } else {
             // in-between wait between steps
@@ -98,7 +102,7 @@ void GAXSync_render(GAX_player* player, int unk) {
             player->step_finished = FALSE; // step is not finished yet
         }
         
-        if (player->speed_buf == 0) {
+        if ((player->speed_buf & 0xFF) == 0) {
             player->songend = TRUE;
         }
         
@@ -106,9 +110,13 @@ void GAXSync_render(GAX_player* player, int unk) {
         if (player->unk0 == 0) {
             player->unk0 = unk;
         }
+        
         if (player->unknown_delay != 0) {
             player->unknown_delay--;
         }
         
     }
+    
+    return 0;
+
 }
